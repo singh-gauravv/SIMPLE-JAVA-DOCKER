97b30c70f09cbc6640bebbdeced71f726dda4154
singh-gauravv <gauravsunil2005@gmail.com>
2025-04-29 11:15:39 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Docker! Current date: " + currentDate);
+    }
+}
+
